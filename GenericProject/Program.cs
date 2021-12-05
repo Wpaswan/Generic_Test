877bdf541b80e48617fa348be3d5ae3abb11fa7a
Wpaswan <dablumcapaswan@gmail.com>
2021-12-04 17:02:59 -0800

    [Dablu, added the code to compare to three numbers]

--- GenericProject/Program.cs
+++ GenericProject/Program.cs
@@ -1,2 +1,39 @@
-﻿
-Console.WriteLine("Welcome to generic test project!!");
+﻿class Comparison
+{
+    public static int FindMaxIntNumber(int first, int second, int third)
+    {
+        if ((first.CompareTo(second) >0)&&(first.CompareTo(third)>0))
+        {
+            return first;
+        }
+        else if ((second.CompareTo(first)>0)&&(second.CompareTo(third)>0))
+        {
+            return second;
+        }
+        else if ((third.CompareTo(second)>0)&&(third.CompareTo(first)>0))
+        {
+            return third;
+        }
+        else
+        {
+
+            return default;
+        }
+    }
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter three numbers:");
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        int c = Convert.ToInt32(Console.ReadLine());
+        int max = FindMaxIntNumber(a, b, c);
+        if (max==0)
+        {
+            Console.WriteLine("All three numbers are equal!!!");
+        }
+        else
+        {
+            Console.WriteLine("Maximum among three:"+max);
+        }
+    }
+}
