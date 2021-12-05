7c0e139dad86df082707d01855418c05a1569571
Wpaswan <dablumcapaswan@gmail.com>
2021-12-04 19:09:27 -0800

    [Dablu, added code using generic]

--- GenericProject/Program.cs
+++ GenericProject/Program.cs
@@ -1,42 +1,6 @@
-﻿class Comparison
-{
-    public static string FindMaxFloatNumber(string first, string second, string third)
-    {
-        if ((first.CompareTo(second) >0)&&(first.CompareTo(third)>0))
-        {
-            return first;
-        }
-        else if ((second.CompareTo(first)>0)&&(second.CompareTo(third)>0))
-        {
-            return second;
-        }
-        else if ((third.CompareTo(second)>0)&&(third.CompareTo(first)>0))
-        {
-            return third;
-        }
-        else
-        {
+﻿using GenericProject;
 
-            return default;
-        }
-    }
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Enter three words:");
-        string word1 = Console.ReadLine();
-        string word2 = Console.ReadLine();
-        string word3 = Console.ReadLine();
-
-
-        string maxString = FindMaxFloatNumber(word1, word2, word3);
-
-        if (maxString==null)
-        {
-            Console.WriteLine("All three numbers are equal!!!");
-        }
-        else
-        {
-            Console.WriteLine("Maximum among three:"+maxString);
-        }
-    }
-}
+            Generic<int> first = new Generic<int>();
+            first.FindMax(1, 2, 3);
+        
+    
